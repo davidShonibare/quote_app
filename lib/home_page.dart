f446ef2e5d4ee7e358007c9470ea1b877143544b
davidShonibare <david.shonibare@kodehauz.com>
2025-05-31 00:58:57 +0100

    modified location of favorite star icon

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,73 +9,91 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Daily Quotes')),
-        body: Obx(
-          () {
-            if (controller.isLoading.value) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (controller.quote.value == null) {
-              return const Center(child: Text('Error: Failed to load quote'));
-            } else {
-              return Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Stack(
-                  children: [
-                    Align(
-                      alignment: Alignment.center,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            '"${controller.quote.value!.quote}"',
-                            style: const TextStyle(
-                              fontSize: 32,
-                            ),
-                            textAlign: TextAlign.center,
+      appBar: AppBar(
+        title: const Text('Daily Quotes'),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: Obx(
+              () => GestureDetector(onTap: controller.toggleFavorites,
+                child: Icon(
+                    !controller.isFav.value
+                        ? Icons.star_border_outlined
+                        : Icons.star,
+                    size: 36,color: controller.isFav.value?Colors.red: const Color.fromARGB(18, 0, 0, 0),),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Obx(
+        () {
+          if (controller.isLoading.value) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (controller.quote.value == null) {
+            return const Center(child: Text('Error: Failed to load quote'));
+          } else {
+            return Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Stack(
+                children: [
+                  Align(
+                    alignment: Alignment.center,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          '"${controller.quote.value!.quote}"',
+                          style: const TextStyle(
+                            fontSize: 32,
                           ),
-                          Padding(
-                            padding: const EdgeInsets.only(right: 20),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                Text(
-                                  "- ${controller.quote.value!.writer}",
-                                  style: const TextStyle(
-                                    fontStyle: FontStyle.italic,
-                                    fontSize: 14,
-                                  ),
-                                  textAlign: TextAlign.right,
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                     Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Padding(
-                        padding:const EdgeInsets.only(bottom: 60.0),
-                        child: GestureDetector( onTap: controller.toggleFavorites,
+                          textAlign: TextAlign.center,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 20),
                           child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.end,
                             children: [
                               Text(
-                                !controller.isFav.value?'Add to Favorites':'Remove from Favorites',
-                                style: TextStyle(fontSize: 24),
+                                "- ${controller.quote.value!.writer}",
+                                style: const TextStyle(
+                                  fontStyle: FontStyle.italic,
+                                  fontSize: 14,
+                                ),
+                                textAlign: TextAlign.right,
                               ),
-                             const SizedBox(width: 10),
-                              Icon(!controller.isFav.value? Icons.star_border_outlined:Icons.star, size: 30)
                             ],
                           ),
                         ),
+                      ],
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 60.0),
+                      child: GestureDetector(
+                        onTap: controller.toggleFavorites,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              !controller.isFav.value
+                                  ? 'Add to Favorites'
+                                  : 'Remove from Favorites',
+                              style: TextStyle(fontSize: 24),
+                            ),
+                          ],
+                        ),
                       ),
-                    )
-                  ],
-                ),
-              );
-            }
-          },
-        ));
+                    ),
+                  )
+                ],
+              ),
+            );
+          }
+        },
+      ),
+    );
   }
 }
